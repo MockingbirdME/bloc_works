fa57bdec8f86aae7a0af6c14e99a6c00075c764c
Devon Henegar <devon.henegar@gmail.com>
2017-04-17 10:36:47 -0400

    added tests

--- spec/tests.rb
+++ spec/tests.rb
@@ -10,10 +10,18 @@ class BlocWorksTest < Test::Unit::TestCase
   end
 
   test "it should be hooked up properly" do
-    get "/" 
+    get "/"
     assert last_response.ok?
     assert last_response.body.include?("Hello Blocheads!")
     assert_equal "Hello Blocheads!", last_response.body
+  end
+
+  Test "it should provide controller and action through the router" do
+    get "/books/welcome"
+    assert last_response.ok?
+    assert_equal(200, last_response.status)
+    assert_equal("text/html", last_response.content_type)
+    assert_equal("Hello Blocheads!", last_response.body)
   end
 
 end
